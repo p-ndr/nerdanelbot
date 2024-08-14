18b1dcdc232ff122c4c4b3e5a3ce8fd1f6f73133
Parnian Naderi <negarintarlan@gmail.com>
2024-08-14 13:55:07 +0330

    defined a few more tokens.

--- main.py
+++ main.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv
 load_dotenv()
 
 my_token = os.getenv('TOKEN')
+reading_timeout = os.getenv('READ_TIMEOUT')
+update_time = os.getenv('UPDATE_AFTER_TIMEOUT')
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -14,11 +16,15 @@ logging.basicConfig(
 )
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    print(context.user_data)
     _text = "سلام! من نردانلم. لطفا پیام خود را بگذارید!"
     await context.bot.send_message(chat_id=update.effective_chat.id, text=_text)
 
+# async def receive_message(update: Update,  context: ContextTypes.DEFAULT_TYPE):
+#     msg = update 
+
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(my_token).read_timeout(15).get_updates_read_timeout(42).build()
+    application = ApplicationBuilder().token(my_token).read_timeout(read_timeout=reading_timeout).get_updates_read_timeout(get_updates_read_timeout=update_time).build()
     
     start_handler = CommandHandler('start', start)
     application.add_handler(start_handler)

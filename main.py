b0d938342b8e33724b81de947f6bc752e099076a
Parnian Naderi <negarintarlan@gmail.com>
2024-08-14 19:58:44 +0330

    reverted to using telebot instead of PTB. Git misuse again...

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
 import os
-import logging
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import telebot 
 from dotenv import load_dotenv 
 
 load_dotenv()
@@ -10,23 +8,13 @@ my_token = os.getenv('TOKEN')
 reading_timeout = os.getenv('READ_TIMEOUT')
 update_time = os.getenv('UPDATE_AFTER_TIMEOUT')
 
-logging.basicConfig(
-    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-    level=logging.INFO
-)
+bot = telebot.TeleBot(my_token)
 
-async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    # print(context.user_data)
-    _text = "سلام! من نردانلم. لطفا پیام خود را بگذارید!"
-    await context.bot.send_message(chat_id=update.effective_chat.id, text=_text)
 
-# async def receive_message(update: Update,  context: ContextTypes.DEFAULT_TYPE):
-#     msg = update 
+# Handle '/start' and '/help'
+@bot.message_handler(commands=['start'])
+def send_welcome(message):
+    bot.reply_to(message, "سلام! نردانلم. لطفا پیام خود را بگذارید!")
 
-if __name__ == '__main__':
-    application = ApplicationBuilder().token(my_token).read_timeout(read_timeout=reading_timeout).get_updates_read_timeout(get_updates_read_timeout=update_time).build()
-    
-    start_handler = CommandHandler('start', start)
-    application.add_handler(start_handler)
-    
-    application.run_polling()
+
+bot.infinity_polling()
